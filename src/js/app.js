8731ac619b6c1dbe935ee3e85c44714e824dbf06
Svetlana <pokrowsckaya.sveta@gmail.com>
2023-03-04 18:49:24 +0300

    добавлен блок футер, about, слайдер, адаптив

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import { isWebp, openPopUpActions, closePopUpActions, filterPriceForm, init } from './modules'
+import Swiper, { EffectFlip, Thumbs } from 'swiper';
 import AOS from 'aos'
 
 window['FLS'] = true
@@ -11,3 +12,20 @@ filterPriceForm()
 init()
 
 AOS.init();
+
+var swiper = new Swiper(".my-swiper", {
+   loop: true,
+   spaceBetween: 10,
+   slidesPerView: 4,
+   freeMode: true,
+   watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".my-swiper2", {
+   modules: [EffectFlip, Thumbs],
+   effect: 'flip',
+   loop: true,
+   spaceBetween: 10,
+   thumbs: {
+      swiper: swiper,
+   },
+});
